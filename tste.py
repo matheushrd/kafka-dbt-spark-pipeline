8d561df019188d6868e4c553e2808294d425a495
Matheus <matheushrd@hotmail.com>
2024-01-07 18:39:55 -0300

    setting uop trino

--- tste.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType, BooleanType
-
-spark = SparkSession.builder.appName("HudiKafkaConsumer").getOrCreate()
-
-spark.stop()
